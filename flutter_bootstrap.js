8cc0056e79ce00632026c95b1d526022ac805302
Shredds0905 <shraddhagolhar0@gmail.com>
2026-07-04 18:22:06 +0530

    Built web
    Branch: main
    Commit: 96ce77a8a38bca81a10dfe353039c893cdac10e6 (dirty)
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1706804774"
+    serviceWorkerVersion: "3940465887"
   }
 });
